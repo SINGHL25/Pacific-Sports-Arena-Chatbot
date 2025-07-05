17c6eeea419e1ffbe43c79fb73bc1d57ae08e6aa
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 07:12:31 +1000

    Update email_utils.py

--- email_utils.py
+++ email_utils.py
@@ -1,1 +1,34 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+def send_booking_email(data):
+    try:
+        user = os.getenv("EMAIL_USER")
+        pwd = os.getenv("EMAIL_PASS")
+        to = os.getenv("EMAIL_OWNER")
+
+        message = f"""
+New Booking at Pacific Sports Arena:
+
+Name: {data['name']}
+Contact: {data['contact']}
+Activity: {data['activity']}
+Date: {data['date']}
+Time: {data['time']}
+Feedback: {data['feedback']}
+"""
+
+        msg = MIMEText(message)
+        msg["Subject"] = "📬 New Booking Notification"
+        msg["From"] = user
+        msg["To"] = to
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(user, pwd)
+            server.send_message(msg)
+
+    except Exception as e:
+        print("Email Error:", e)
 
